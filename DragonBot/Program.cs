fba71bd2c838cc4b8962f0f513b7b14837cf5a4b
Logan Payton <Logan.Payton@javs.com>
2019-05-06 21:02:06 -0400

    Add project files.

---
+++ DragonBot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using CommonServiceLocator;
+using Discord.WebSocket;
+using DragonStallion.Common.ServiceLocation;
+
+namespace DragonBot
+{
+    internal static class Program
+    {
+        private static async Task Main()
+        {
+            var serviceLocator = BuildServiceLocator();
+            var service = serviceLocator.GetInstance<Service>();
+
+            await service.StartAsync(Environment.GetEnvironmentVariable("DISCORD_TOKEN"));
+
+            // Wait for escape
+            while (Console.ReadKey().Key != ConsoleKey.Escape)
+            {
+            }
+
+            await service.StopAsync();
+            service.Dispose();
+        }
+
+        private static IServiceLocator BuildServiceLocator() =>
+            new ServiceLocatorFactory()
+                .WithAssemblyTypes(typeof(DiscordSocketClient))
+                .Build();
+    }
+}
---
+++ DragonBot/Service.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace DragonBot
+{
+    public class Service : IDisposable
+    {
+        private readonly DiscordSocketClient _client;
+
+        public Service(DiscordSocketClient client)
+        {
+            _client = client;
+            _client.Log += Client_Log;
+            _client.Ready += Client_Ready;
+            _client.MessageReceived += Client_MessageReceived;
+        }
+
+        public async Task StartAsync(string token)
+        {
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+        }
+
+        public async Task StopAsync()
+        {
+            await _client.StopAsync();
+            await _client.LogoutAsync();
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+
+        private Task Client_Log(LogMessage message)
+        {
+            return Console.Out.WriteLineAsync(message.ToString());
+        }
+
+        private Task Client_Ready()
+        {
+            return Console.Out.WriteLineAsync($"Connected with User {_client.CurrentUser.Username}");
+        }
+
+        private async Task Client_MessageReceived(SocketMessage message)
+        {
+            if (message.Author.Id == _client.CurrentUser.Id) return;
+
+            await Console.Out.WriteLineAsync(message.ToString());
+            await Task.CompletedTask;
+        }
+    }
+}
